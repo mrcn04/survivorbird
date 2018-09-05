3d754757431dc29513f87e0dcce5663308797bc7
mrcn04 <37182227+mrcn04@users.noreply.github.com>
2018-09-06 01:31:58 +0300

    Initial commit

---
+++ SurvivorBird/android/src/com/ocengiz/survivorbird/AndroidLauncher.java
@@ -0,0 +1,19 @@
+package com.ocengiz.survivorbird;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+
+public class AndroidLauncher extends AndroidApplication {
+
+	private static int SPLASH_TIME_OUT = 3000;
+
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new SurvivorBird(), config);
+
+	}
+}
